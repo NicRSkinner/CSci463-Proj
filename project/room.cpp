5c69d65c28b9a1e860fcab6b2a0829a9c62ff596
David Johnson <davidjjdj@protonmail.com>
2019-04-17 15:36:46 -0500

    More class fixes, soon to be merged with seths
    Some class fixes with the room class

--- project/room.cpp
+++ project/room.cpp
@@ -1,6 +1,6 @@
 #include "room.h"
 
-extern bool securityAlarm, fireAlarm, signs, controlRoom;
+//bool securityAlarm, fireAlarm, signs;
 
 /*
 Room()
@@ -15,45 +15,52 @@ Room()
 
 }
 */
-void setSecurityAlarm(bool value) {
-    securityAlarm = value;
+
+void Room::activateFireAlarm() {
+    fireAlarm = true;
+}
+void Room::deactivateFireAlarm() {
+    fireAlarm = false;
 }
 
-void setAlarm(bool value) {
-    fireAlarm = value;
+void Room::activateSecurityAlarm() {
+    securityAlarm = true;
 }
 
-void setDoor(Door door) {
+void Room::deactivateSecurityAlarm() {
+    securityAlarm = false;
+}
+
+void Room::addDoor(Door door) {
     //instanciate door
     //door = ;
+    Room::doors.append(door);
 }
 
-void setZone(Zone zone) {
+void Room::setZone(Zone zone) {
     //instanciate zone room is in
     //room = ;
+    Room::zone = &zone;
 }
 
-void setSign(bool sign) {
+void Room::setSign(bool sign) {
     signs = sign;
 }
 
-void SetContrtolRoom(bool room) {
-    controlRoom = room;
-}
-
-bool getSecurityAlarm() {
+bool Room::getSecurityAlarmState() {
     return securityAlarm;
 }
 
-bool getFireAlarm(){
+bool Room::getFireAlarmState(){
     return fireAlarm;
 }
-bool getDoor() {
+
+QList<Door> Room::getDoors() {
     //return door
+    return Room::doors;
 }
-bool getZone() {
+void Room::getZone() {
     //return zone
+    return Room::zone;
 }
-bool getControlRoom() {
-    return controlRoom;
-}
+*/
