848d86755dd017d2fafbd7e01203134780f45a5e
speri203 <peri.sai203@gmail.com>
2019-04-22 20:14:01 -0500

    Added signal and slots skeleton for testbench. Works with emits and time based

--- project/testbench.cpp
+++ project/testbench.cpp
@@ -2,6 +2,7 @@
 #include "ui_testbench.h"
 #include "door.h"
 #include <QTimer>
+#include <QDebug>
 
 testBench::testBench(QWidget *parent) :
     QDialog(parent),
@@ -27,12 +28,35 @@ void testBench::programLoop() {
     //Need to set up the QTimer
 
     //Create class instant for each object
-
-
-
+    this->triggerAlarm();
+    connect(this, SIGNAL(ready()), this, SLOT(close()));
+    // Initial test, closes program after 10 seconds
     QTimer *timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(on_pushButtonClose_clicked()));
+    connect(timer, SIGNAL(timeout()), this, SLOT(triggerAlarm()));
     timer->start(10000); //Ready to emit signal
+
+    //This is to trigger alarm after x seconds
+    //connect(timer, SIGNAL(timeout()), this, SLOT(triggerAlarm()));
+    //timer->start(1000);
+
+    //This is to trigger alarm after x seconds
+    //connect(timer, SIGNAL(timeout()), this, SLOT(triggerAlarm()));
+    //timer->start(1000);
+
+
+
+}
+
+void testBench::close() {
+    this->close();
+}
+
+void testBench::triggerAlarm() {
+    //This is where the function for triggering alarm will be. Needs to be setup
+    qDebug() << "Here next closing";
     emit ready();
+}
+
+void testBench::lockDoor() {
 
 }
